8ee396c34af99e0413c297ef9169ade5ff00eb2e
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-10-11 01:10:17 +0700

    Project SEM 4

---
+++ src/main/java/com/example/carforum/CarForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.carforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carforum/CarForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
