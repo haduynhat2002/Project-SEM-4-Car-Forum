cf94e2efed15d0bb68f2de2cd1d3f61e91aa6ebc
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-10-19 22:33:26 +0700

    crud topic

--- src/main/java/com/example/carforum/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.carforum.repository;
-
-import com.example.carforum.entity.Role;
-import com.example.carforum.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
--- src/main/java/com/example/carforum/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.carforum.repository;
-
-import com.example.carforum.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
