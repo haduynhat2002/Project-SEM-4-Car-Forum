5f90b8a98f3bc7b8d6295f06ea2135c4f139402d
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-10-22 00:28:47 +0700

    post

--- src/main/java/com/example/carforum/entity/Post.java
+++ src/main/java/com/example/carforum/entity/Post.java
@@ -1,38 +1,30 @@
 package com.example.carforum.entity;
 
-import com.example.carforum.entity.myenum.UserStatus;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.sql.Date;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @Table(name = "posts")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(length = 125, nullable = false, unique = true)
     private String title;
     private String content;
-    private LocalDateTime date;
+    private LocalDateTime dateTime;
     private String image;
-    @JsonIgnore
     @ManyToOne
-    @JoinColumn(name="Topic_id", nullable = false)
+    @JoinColumn(name = "topic_id")
     private Topic topic;
-    @Column(updatable = false, insertable = false)
-    private int topic_id;
-    private String description;
-    @Enumerated(EnumType.ORDINAL)
-    private UserStatus status;
 
 }
--- src/main/java/com/example/carforum/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.carforum.repository;
-
-import com.example.carforum.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
