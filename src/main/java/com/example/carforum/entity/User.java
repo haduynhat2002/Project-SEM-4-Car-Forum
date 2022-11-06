72e6acc06ac538601d56d5178be6e384a243b9db
hieu130 <hieuvanphan275@gmail.com>
2022-11-06 20:45:18 +0700

    edit update

--- src/main/java/com/example/carforum/entity/User.java
+++ src/main/java/com/example/carforum/entity/User.java
@@ -5,6 +5,10 @@ package com.example.carforum.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +24,36 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "Name")
+    @NotNull(message ="Vui lòng nhập tên")
+    @Size(min = 2, max = 30, message = "Tên của bạn phải nhiều hơn 2 lí tự và không quá 30 kí tự")
     private String fullname;
+
+    @NotNull(message ="Vui lòng nhập giới tính")
     private String gender;
+
+    @NotNull(message = "")
     private String dob;
+
+    @NotNull(message = "Vui lòng nhập Email")
+    @Email(message ="Vui lòng nhập đúng Email")
     private String email;
+
+    @NotNull(message = "Vui lòng nhập số điện thoại")
+    @Size(max = 10, min = 10, message = "Số điện thoại phải có 10 chữ số")
+//    @Pattern(regexp = "[7-9][0-9]{9}", message = "Số điện thoại không hợp lệ")
     private String phone;
+
+    @NotNull(message = "Vui lòng nhập ảnh đại diện")
     private String avatar;
+
+    @NotNull(message = "Vui lòng nhập địa chỉ")
     private String address;
+
+//    @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})",
+//            message = "Enter valid password")
+    @NotNull(message = "Vui lòng nhập mật khẩu")
     private String password;
     private int status;
     @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
