946127ef6dfd7de289d0b714628c10b5be32d7a3
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-11-16 02:05:54 +0700

    form-comment

--- src/main/java/com/example/carforum/repository/UserRepository.java
+++ src/main/java/com/example/carforum/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
      public User findByUserName(String username);
-
+     Optional<User> findUserById(Integer id);
 }
